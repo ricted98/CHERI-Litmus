eafc178a0098aee95d70e92e659084aee1adf4da
msc22h2 <s290705@studenti.polito.it>
2022-12-07 17:42:55 +0100

    Better log information

--- binaries/make-riscv.sh
+++ binaries/make-riscv.sh
@@ -7,32 +7,52 @@ fi
 
 sed "25 s/  uint32_t seed = [0-9];/  uint32_t seed = $RANDOM;/" ../main.copy > ../backend/main.c
 rm -rf backend-tmp
+n_tests=0
+n_compiled_tests=0
+n_parsing_errors=0
+n_more_process=0
+n_compiling_errors=0
+n_no_opcode=0
 i=0
-j=0
 
 for FILE in $(find $1 -name "*.litmus") #| grep -v "BASIC_2_THREAD\|AMO_X0_2_THREAD")
  do
   # if [ $i -lt 1 ]
   # then
-  #FILE="/scratch/msc22h2/CHERI-Litmus/tests/riscv-tests/non-mixed-size/RELAX/Coi-Rfi/MP+fence.rw.rw+ctrl-wsi-rfi-addr.litmus"
+  # FILE="/scratch/msc22h2/cva6-litmus/tests/riscv-tests/non-mixed-size/FENCE.TSO/2/2+2W+fence.tso+fence.tsoxp.litmus"
   echo "Processing file $FILE"
-  cp -r ../backend/ backend-tmp
-  if ../frontend/litmus $FILE backend-tmp/testcase.c backend-tmp/testcase.h $2 && ! grep -q P2 $FILE
-  then
-   cd backend-tmp
-   ./make-riscv.sh
-   cd ..
-   OUTFILE=`basename $FILE .litmus`.elf
-   if cp backend-tmp/main.elf $OUTFILE; then
-       OUTFILE=`basename $FILE .litmus`.dump
-       cp backend-tmp/main.dump $OUTFILE
-       rm -rf backend-tmp
-       j=$((j+1))
-   fi
-  # fi
-  i=$((i+1))
+  if ! grep -q sw.rl $FILE && ! grep -q lw.aq $FILE;then
+    cp -r ../backend/ backend-tmp
+    if ../frontend/litmus $FILE backend-tmp/testcase.c backend-tmp/testcase.h $2; then
+      if ! grep -q P2 $FILE; then
+         cd backend-tmp
+         ./make-riscv.sh
+         cd ..
+         OUTFILE=`basename $FILE .litmus`.elf
+         if cp backend-tmp/main.elf $OUTFILE; then
+             OUTFILE=`basename $FILE .litmus`.dump
+             cp backend-tmp/main.dump $OUTFILE
+             n_compiled_tests=$((n_compiled_tests+1))
+         else
+            n_compiling_errors=$((n_compiled_errors+1))
+         fi
+      else 
+        n_more_process=$((n_more_process+1))
+      fi
+    else
+      n_parsing_errors=$((n_parsing_errors+1))
+    fi
+  else
+    n_no_opcode=$((n_no_opcode+1))
   fi
-  
+  rm -rf backend-tmp
+  n_tests=$((n_tests+1))
+  # fi 
+  # i=$((i+1))
  done 
- echo "#file well compiled = $j"
- echo "#file analized      = $i"
+ echo "#tests               = $n_tests"
+ echo "#tests well compiled = $n_compiled_tests"
+ echo "#parsing errors      = $n_parsing_errors"
+ echo "#too much precesses  = $n_more_process"
+ echo "#compiling errors    = $n_compiling_errors"
+ echo "#lw.aq or sw.rl      = $n_no_opcode"
