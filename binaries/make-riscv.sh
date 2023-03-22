fd13a198680dae9114af55ab69862dfcc96b874c
msc22h2 <s290705@studenti.polito.it>
2023-03-22 18:04:01 +0100

    Comments added

--- binaries/make-riscv.sh
+++ binaries/make-riscv.sh
@@ -10,23 +10,19 @@ rm -rf backend-tmp
 n_tests=0
 n_compiled_tests=0
 n_parsing_errors=0
-n_more_process=0
 n_compiling_errors=0
+#refers to the lw.aq and sw.rl that are still not 
+#defined by riscv spec
 n_no_opcode=0
 i=0
 
 #ATOMICS/CO and HAND litmus tests can't actually be compiled
 for FILE in $(find $1 -name "*.litmus" | grep -v "ATOMICS/CO\|HAND")
  do
-  # if [ $i -lt 1 ]
-  # then
-  # FILE="../tests/riscv-tests/non-mixed-size/SAFE/IRIW+addr+ctrlfencei.litmus"
   echo "Processing file $FILE"
   if ! grep -q sw.rl $FILE && ! grep -q lw.aq $FILE;then
     cp -r ../backend/ backend-tmp
-    #if grep -q P0 $FILE; then
       if ../frontend/litmus $FILE backend-tmp/testcase.c backend-tmp/testcase.h $2; then
-        #if grep -q P3 $FILE; then
            cd backend-tmp
            ./make-riscv.sh
            cd ..
@@ -38,24 +34,17 @@ for FILE in $(find $1 -name "*.litmus" | grep -v "ATOMICS/CO\|HAND")
            else
               n_compiling_errors=$((n_compiled_errors+1))
            fi
-        # else 
-        #   n_more_process=$((n_more_process+1))
-        # fi
       else
         n_parsing_errors=$((n_parsing_errors+1))
       fi
-    #fi
   else
     n_no_opcode=$((n_no_opcode+1))
   fi
   rm -rf backend-tmp
   n_tests=$((n_tests+1))
-  #fi 
-  i=$((i+1))
  done 
  echo "#tests               = $n_tests"
  echo "#tests well compiled = $n_compiled_tests"
  echo "#parsing errors      = $n_parsing_errors"
- echo "#too much precesses  = $n_more_process"
  echo "#compiling errors    = $n_compiling_errors"
  echo "#lw.aq or sw.rl      = $n_no_opcode"
--- ci/merge-tests.sh
+++ ci/merge-tests.sh
@@ -6,19 +6,18 @@ source=./log
 rm $dest
 touch $dest
 i=0
-for FILE in $(ls $source) #$(ls log | find ../CHERI-Litmus/tests/riscv-tests/non-mixed-size/ | grep Coi-Rfi)
+for FILE in $(ls $source) 
 do 
-	#FILE=$(basename $FILE .litmus).log
 
-	#if ! find ../cva6-litmus/tests/riscv-tests/non-mixed-size/ -name "$(basename $FILE .log).litmus" | grep -q HAND; then
-		if grep -q Time $source/$FILE; then 
-			cat $source/$FILE >> $dest
-			echo "" >> $dest
-			i=$((i+1))
-		else
-			echo "'Time' not found in $FILE"
-		fi
-	#fi
+	#check that the litmus test terminated correctly
+	if grep -q Time $source/$FILE; then 
+		cat $source/$FILE >> $dest
+		echo "" >> $dest
+		i=$((i+1))
+	else
+		echo "'Time' not found in $FILE"
+	fi
+
 
 done 
 echo "Num of tests = $i"
