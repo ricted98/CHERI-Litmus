7fab4cdc0ecceee0cbc6bf538c643c17a0171766
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-07-01 01:25:01 +0200

    compare_model.sh: Fix paths
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- ci/compare_model.sh
+++ ci/compare_model.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-model_result=../litmus-tests-riscv/model-results/herd.logs
-hw_result=run.log
+[ -z "$MCMP7" ] && MCMP7="mcompare7"
 
-mcompare7 -nohash $hw_result $model_result
+model_result=model-results/herd.logs
+hw_result=../../litmus.log
+
+$MCMP7 -nohash $hw_result $model_result
