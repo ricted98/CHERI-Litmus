1f65ea9be49552860e19790aba667c260dd45a1b
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-08-28 12:12:40 +0200

    compare_model.sh: Support custom log file path
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- ci/compare_model.sh
+++ ci/compare_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 [ -z "$MCMP7" ] && MCMP7="mcompare7"
+[ -z "LITMUS_LOG" ] && LITMUS_LOG=../../litmus.log
 
 model_result=model-results/herd.logs
-hw_result=../../litmus.log
 
-$MCMP7 -nohash $hw_result $model_result
+$MCMP7 -nohash $LITMUS_LOG $model_result
