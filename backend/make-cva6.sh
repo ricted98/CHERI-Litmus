0195ddfb6cc6373bacf99e8c0a752404d59a5099
bluew <bluewww@users.noreply.github.com>
2023-10-03 14:04:58 +0200

    cva6: Fix missing include path to $PLATFORM directory

--- backend/make-cva6.sh
+++ backend/make-cva6.sh
@@ -7,7 +7,7 @@ OBJCOPY="/usr/pack/riscv-1.0-kgf/riscv64-gcc-8.5.0/bin/riscv64-unknown-elf-objco
 OBJDUMP="/usr/pack/riscv-1.0-kgf/riscv64-gcc-8.5.0/bin/riscv64-unknown-elf-objdump"
 
 OPT="-O -fno-builtin"          #optimize even more and avoid to use standard c functions
-CFLAGS="$OPT -I. -mcmodel=medany -g"  #include actuar directory to search directories
+CFLAGS="$OPT -I. -I$PLATFORM -mcmodel=medany -g"  #include actuar directory to search directories
 LDFLAGS="-G 0 -T $PLATFORM/$PLATFORM.ld"
 
 CFILES="main io log hash rand riscv/arch cva6/platform cva6/uart test"
@@ -21,4 +21,4 @@ done
 $AS -o entry.o cva6/entry.s
 $LD $LDFLAGS -o main.elf entry.o $OFILES
 $OBJDUMP -S main.elf > main.dump
-#elf2hex 16 65536 main.elf > 
+#elf2hex 16 65536 main.elf >
